25ff007732e77c2e1b8ccaa8aa961e849ee40bf8
RAVIPATI <RAVIPATI-VASANTH>
2022-12-10 22:30:25 +0530

    made necessary files for managing panel

--- client/src/body/managing/managing.js
+++ client/src/body/managing/managing.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
+import SearchPanel from "../billing/searchPanel/searchpanel";
+import OperatePanel from "./operatePanel/operatePanel";
 
 export default class Managing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listedItems: [],
+    };
+  }
+
+  updateListedItems(newItem) {
+    let l = this.state.listedItems;
+    l.push(newItem);
+    this.setState({
+      listedItems: l,
+    });
+  }
+
   render() {
-    return <h1>Managing</h1>;
+    return (
+      <>
+        <SearchPanel updateListedItems={this.updateListedItems.bind(this)} />
+        <OperatePanel />
+      </>
+    );
   }
 }
