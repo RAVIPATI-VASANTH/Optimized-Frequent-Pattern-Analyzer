fd908af8af476f23f4f45a364458013ea7554ce6
RAVIPATI-VASANTH <vasanthravipati99@gmail.com>
2023-01-04 09:58:26 +0530

    m

--- server/controllers/taskControllers/cancelFPARequestController.js
+++ server/controllers/taskControllers/cancelFPARequestController.js
@@ -9,10 +9,8 @@ const cancelFPARequestController=(req,res)=>{
             try {
                 const database = client.db("MBAProjectDatabase");
                 const retailerRequests = database.collection("retailerRequests");
-                console.log(req.query.userId,req.query.requestName);
                 await retailerRequests.deleteOne({userId:req.query.userId,requestName:req.query.requestName})
-                console.log("called");
-            resolve();
+                resolve();
           } finally {
           }
         }
